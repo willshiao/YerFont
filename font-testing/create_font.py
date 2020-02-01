285f5b4ef11da2373a84262de525ef1852e2a639
William Shiao <willshiao@gmail.com>
2020-02-01 01:32:18 -0800

    Add letter-specific vertical spacing

--- font-testing/create_font.py
+++ font-testing/create_font.py
@@ -1,34 +1,43 @@
 import fontforge
 import psMat
 
-def normalizeGlyph(g):
+SPECIAL_CHARS = set('gjpqy')
+
+def normalizeGlyph(g, letter):
     bb = g.boundingBox()
     dy = bb[3]-bb[1]
     newScale = 800/(dy)
     transforms = []
-    transforms.append(psMat.translate(-bb[0], -bb[1]))
+    if letter in SPECIAL_CHARS:
+        print('SPECIAL CASE: ', letter)
+        print(bb)
+        transforms.append(psMat.translate(-bb[0], -bb[1] - 0.5 * (bb[3] - bb[1])))
+    else:
+        transforms.append(psMat.translate(-bb[0], -bb[1]))
+
     if dy > 800:
         transforms.append(psMat.scale(newScale))
     if len(transforms) > 1:
         g.transform(psMat.compose(*transforms))
     else:
         g.transform(transforms[0])
+    print(g.boundingBox())
     return g
 
 
 font = fontforge.font()
-glyph = font.createMappedChar('A')
-glyph.importOutlines('a.svg')
-normalizeGlyph(glyph)
+# glyph = font.createMappedChar('A')
+# glyph.importOutlines('a.svg')
+# normalizeGlyph(glyph)
 
 glyph = font.createMappedChar('B')
 glyph.importOutlines('yin_yang.svg')
-normalizeGlyph(glyph)
+normalizeGlyph(glyph, 'B')
 
 for i in range(26):
     ltr = chr(ord('a') + i)
     glyph = font.createMappedChar(ltr)
     glyph.importOutlines(f'letter_{i}_clean.svg')
-    normalizeGlyph(glyph)
+    normalizeGlyph(glyph, ltr)
 
 font.generate('testfont.ttf')
